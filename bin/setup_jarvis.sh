076d5fc6f07f65432af43a8786d07b697988edaf
Mizuki Sango <msageha@gmail.com>
2022-03-19 03:37:40 +0900

    fix

--- bin/setup_jarvis.sh
+++ bin/setup_jarvis.sh
@@ -1,3 +1,5 @@
+gcloud artifacts print-settings python --project=jarvis-ml-dev  --repository jarvis-ml-python-repo --location us-west4
+ln -sfn $HOME/Works/src/github.com/mzk622/dotfiles/jarvis/.pypirc $HOME/
 
 # for airflow
 ghq get git@bitbucket.org:jarvisml/airflow.git
@@ -5,7 +7,7 @@ cd ~/Works/src/bitbucket.org/jarvisml/airflow
 python3 -m venv venv
 echo "source venv/bin/activate" >> .envrc
 direnv allow
-pip install twine keyrings.google-artifactregistry-auth
+pip install keyring keyrings.google-artifactregistry-auth
 ln -sfn $HOME/Works/src/github.com/mzk622/dotfiles/jarvis/pip.conf $PWD/venv/
 pip install -r requirements.txt
 
@@ -14,7 +16,7 @@ ghq get git@bitbucket.org:jarvisml/platform.git
 cd ~/Works/src/bitbucket.org/jarvisml/platform
 python3 -m venv venv
 direnv allow
-pip install twine keyrings.google-artifactregistry-auth
+pip install keyring keyrings.google-artifactregistry-auth
 ln -sfn $HOME/Works/src/github.com/mzk622/dotfiles/jarvis/pip.conf $PWD/venv/
 
 
